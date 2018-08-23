d68d635422a59246562cb828fdbb39f9788469fe
Matthew Phillips <matthew@matthewphillips.info>
2018-08-23 13:12:21 -0400

    Decode attributes
    This makes it possible to decode attributes.

--- test/test-childlist.js
+++ test/test-childlist.js
@@ -37,3 +37,41 @@ QUnit.test("Nodes inserted before MutationObserver starts observing", function(a
 	mo.observe(root, { subtree: true, childList: true });
 	article.appendChild(document.createElement("span"));
 });
+
+QUnit.test("Inserting a large stylesheet", function(assert){
+	var done = assert.async();
+
+	var root = document.createElement("div");
+	var main = document.createElement("main");
+	root.appendChild(main);
+	helpers.fixture.el().appendChild(root);
+
+	var encoder = new MutationEncoder(root);
+	var decoder = new MutationDecoder(root.ownerDocument);
+
+	var css = `
+		order-history {
+			background: blue;
+		}
+		/*# sourceURL=/async/orders/orders.css */
+	`;
+
+	var mo = new MutationObserver(function(records) {
+		var bytes = Uint8Array.from(encoder.encode(records));
+		var instr = Array.from(decoder.decode(bytes));
+
+		assert.equal(instr.length, 1, "There was one insertion");
+		assert.equal(instr[0].node.nodeName, "STYLE", "it is a style");
+		assert.equal(instr[0].node.getAttribute("asset-id"), "async/orders/orders.css!done-css@3.0.2#css", "Set the attribute");
+		assert.equal(instr[0].node.textContent, css, "the css was applied");
+
+		done();
+	});
+
+	mo.observe(root, { childList: true, subtree: true});
+
+	var style = document.createElement("style");
+	style.setAttribute("asset-id", "async/orders/orders.css!done-css@3.0.2#css");
+	style.appendChild(document.createTextNode(css));
+	main.appendChild(style);
+});
